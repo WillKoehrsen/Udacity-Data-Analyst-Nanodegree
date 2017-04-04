f3ae4ac6ccef1ba56d93e96a8a3775683809c9b0
Will Koehrsen <wjk68@case.edu>
2017-04-03 20:39:49 -0500

    Submitted P3 for the first time

--- Project Materials/P3 Materials/audit_zip.py
+++ Project Materials/P3 Materials/audit_zip.py
@@ -5,27 +5,28 @@ import mappings
 map_file = 'cleveland_ohio.xml'
 cleveland_zip_codes = mappings.cleveland_zip_codes
 zip_codes = set()
-bad_zips = set()
-suspect_zip_codes = set()
+bad_zips = []
+suspect_zip_codes = []
 
 def audit():
-	print('audit function')
 	for _, elem in ET.iterparse(map_file):
 		if elem.tag == 'tag':
-			if 'zip' in elem.attrib['k']:
+			if 'zip' in elem.attrib['k'] or 'postcode' in elem.attrib['k']:
 				zip = elem.attrib['v']
 				try:
 					zip_codes.add(int(zip))
 				except:
-					bad_zips.add(zip)
+					bad_zips.append(zip)
 
 	for zip_code in zip_codes:
 		if zip_code not in cleveland_zip_codes:
-			suspect_zip_codes.add(zip_code)
+			suspect_zip_codes.append(zip_code)
 
 
 def clean(zip_code):
+	# Easiey way to separate out the problem zip codes
 	if len(zip_code) > 5:
+		# Regular expression splits the code on ; : - 
 		zip_code = re.split('[;:-]', zip_code)
 		try:
 			if int(zip_code[0]) in cleveland_zip_codes:
@@ -45,6 +46,6 @@ def clean(zip_code):
 			if int(zip_code) not in cleveland_zip_codes:
 				zip_code = ''
 		except:
-			zip_code = ''
+			zip_code = ''# Returning an empty string will drop the tag entirely
 	return zip_code
 
